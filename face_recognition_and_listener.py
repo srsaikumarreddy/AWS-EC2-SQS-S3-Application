41694ea0f9764b18d1d6fad797435476c4b5a827
Alekhya Vellanki <punarvasu510@gmail.com>
2022-03-17 16:11:19 -0700

    Update face_recognition_and_listener.py

--- face_recognition_and_listener.py
+++ face_recognition_and_listener.py
@@ -160,8 +160,8 @@ def send_message(sqs,result,output_queue_url):
 if __name__ == "__main__":
 
     AWS_REGION='us-east-1'
-    AWS_ACCESS_KEY="AKIA3DVXYSAZCAPOFO4E"
-    AWS_SECRET_ACCESS_KEY="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"
+    AWS_ACCESS_KEY=""
+    AWS_SECRET_ACCESS_KEY=""
 
     INPUT_QUEUE_URL="https://sqs.us-east-1.amazonaws.com/763815825458/InputQueue"
     OUTPUT_QUEUE_URL="https://sqs.us-east-1.amazonaws.com/763815825458/OutputQueue"
