21a620d7e53b666a7a47038303692c1d13bf9eee
Alekhya Vellanki <punarvasu510@gmail.com>
2022-03-18 21:15:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ AWS_REGION='us-east-1'
 AWS_ACCESS_KEY=""
 AWS_SECRET_ACCESS_KEY=""
 
-INPUT_QUEUE_URL="https://sqs.us-east-1.amazonaws.com/763815825458/InputQueue"
+INPUT_QUEUE_URL=""
 
 sqs = boto3.client("sqs",region_name=AWS_REGION, aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
